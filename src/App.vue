db58d0361d1aef7320800da8dd17330f59f0fcc5
semihataman161 <semihataman16@gmail.com>
2024-10-27 19:19:38 +0300

    Complete HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheNavigation from "./components/nav/TheNavigation.vue";
+</script>
 
 <template>
-  <router-view></router-view>
+  <div class="layout">
+    <the-navigation class="layout__navigation"></the-navigation>
+    <main class="layout__main">
+      <router-view class="layout__content"></router-view>
+    </main>
+  </div>
 </template>
+
+<style lang="scss">
+$font-family: sans-serif;
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: $font-family;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-
+import BaseImage from "../components/UI/BaseImage.vue";
 defineProps<{ msg: string }>();
-
 </script>
 
 <template>
-  <p>Home Page</p>
+  <v-container
+    fluid
+    class="d-flex justify-center align-center"
+    style="height: 100vh"
+  >
+    <v-row class="d-flex justify-center align-center">
+      <v-col cols="12" sm="8" md="6" class="d-flex justify-center align-center">
+        <base-image>
+          <template #image>
+            <img
+              loading="lazy"
+              src="@/assets/age-of-empires.webp"
+              alt="Age of empires"
+            />
+          </template>
+        </base-image>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
