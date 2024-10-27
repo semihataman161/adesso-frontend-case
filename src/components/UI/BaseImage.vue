4973b6765c7bb8a6ec4c30097ee99ec25b27b52d
semihataman161 <semihataman16@gmail.com>
2024-10-27 19:19:16 +0300

    Implement BaseImage component

---
+++ src/components/UI/BaseImage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="base-image">
+    <slot name="image">
+    </slot>
+    <slot name="caption">
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$base-image-caption-color: #555;
+$base-image-max-width: 100%;
+$base-image-font-size: 1rem;
+$base-image-caption-margin: 0.5rem;
+
+.base-image {
+  &__img {
+    max-width: $base-image-max-width;
+    height: auto;
+    display: block;
+  }
+
+  &__caption {
+    margin-top: $base-image-caption-margin;
+    font-size: $base-image-font-size;
+    text-align: center;
+    color: $base-image-caption-color;
+  }
+}
+</style>
