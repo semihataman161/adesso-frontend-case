444093e55056d7d1959c6782742f68157962690b
semihataman161 <semihataman16@gmail.com>
2024-10-27 19:00:48 +0300

    Add Vuetify to the project

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { fileURLToPath } from "url";
+import vuetify from 'vite-plugin-vuetify';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
   build: {
     outDir: "dist",
     emptyOutDir: true,
