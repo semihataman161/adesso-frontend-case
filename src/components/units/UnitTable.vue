e9bfa6b5be36c087814e58c544824eb8855242b1
semihataman161 <semihataman16@gmail.com>
2024-10-28 00:48:44 +0300

    Implement UnitTable to show filtered data in units page

---
+++ src/components/units/UnitTable.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps({
+  tableData: {
+    type: Array,
+    required: true,
+  },
+});
+
+const headers = [
+  { title: "id", value: "id", sortable: true },
+  { title: "name", value: "name", sortable: true },
+  { title: "age", value: "age", sortable: true },
+  { title: "cost", value: "cost", sortable: true },
+];
+
+function getFormattedCostValue(proxy: Proxy<object>): string {
+  const obj = { ...proxy };
+  const entries = Object.entries(obj).map(([key, value]) => `${key}: ${value}`);
+  return entries.join(", ");
+}
+</script>
+
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="props.tableData"
+    item-key="id"
+    items-per-page="5"
+    height="300px"
+  >
+    <template v-slot:item.cost="{ value }">
+      {{ getFormattedCostValue(value) }}
+    </template>
+  </v-data-table>
+</template>
