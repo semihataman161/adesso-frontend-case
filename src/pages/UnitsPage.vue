8e7eb61a209182f7e0eb9c36c9ce2bb8dd284738
semihataman161 <semihataman16@gmail.com>
2024-10-28 04:11:11 +0300

    Make UnitsPage bem compatible and arrange styles

--- src/pages/UnitsPage.vue
+++ src/pages/UnitsPage.vue
@@ -31,24 +31,50 @@ const handleCostFilterChange = (costs: ISelectedCost[]) => {
 </script>
 
 <template>
-  <!-- Age Filter -->
-  <base-card class="base-card">
-    <age-filter @update:selectedAge="handleAgeFilterChange" />
-  </base-card>
+  <div class="unit-filter">
+    <!-- Age Filter -->
+    <base-card class="unit-filter__card">
+      <age-filter
+        class="unit-filter__age"
+        @update:selectedAge="handleAgeFilterChange"
+      />
+    </base-card>
 
-  <!-- Cost Filter -->
-  <base-card class="base-card">
-    <cost-filter @update:selectedCost="handleCostFilterChange" />
-  </base-card>
+    <!-- Cost Filter -->
+    <base-card class="unit-filter__card">
+      <cost-filter
+        class="unit-filter__cost"
+        @update:selectedCost="handleCostFilterChange"
+      />
+    </base-card>
 
-  <!-- Table -->
-  <base-card class="base-card">
-    <unit-table :table-data="filteredTableState.data" />
-  </base-card>
+    <!-- Table -->
+    <base-card class="unit-filter__card">
+      <unit-table
+        class="unit-filter__table"
+        :table-data="filteredTableState.data"
+      />
+    </base-card>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.base-card {
-  margin: 1rem 0;
+.unit-filter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 3rem;
+
+  &__card {
+    margin: 1rem 0;
+    width: 50%;
+  }
+
+  &__age,
+  &__cost,
+  &__table {
+    width: 100%;
+  }
 }
 </style>
