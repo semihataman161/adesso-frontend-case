498a1bdf3f35accc69ed73524e7d1a7b2605ca50
semihataman161 <semihataman16@gmail.com>
2024-10-29 02:02:25 +0300

    Complete unit details page

---
+++ src/components/unit-detail/UnitDetailDisplay.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import BaseCard from "@/components/UI/BaseCard.vue";
+interface UnitDetail {
+  label: string;
+  value: string | number | null;
+}
+
+const props = defineProps<{
+  unitData: UnitDetail[];
+}>();
+</script>
+
+<template>
+  <BaseCard>
+    <ul v-if="unitData.length > 0" class="unit-details__list">
+      <li
+        v-for="(item, index) in props.unitData"
+        :key="index"
+        class="unit-details__item"
+      >
+        <p class="unit-details__text">
+          <strong class="unit-details__label">{{ item.label }}:</strong>
+          {{ item.value || "-" }}
+        </p>
+      </li>
+    </ul>
+  </BaseCard>
+</template>
+
+<style scoped lang="scss">
+.unit-details__list {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+.unit-details__item {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+}
+
+.unit-details__text {
+  margin: 0;
+}
+
+.unit-details__label {
+  font-weight: bold;
+}
+</style>
--- src/pages/UnitDetailPage.vue
+++ src/pages/UnitDetailPage.vue
@@ -1,7 +1,64 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { computed } from "vue";
+import UnitDetailDisplay from "@/components/unit-detail/UnitDetailDisplay.vue";
 
+const route = useRoute();
+const store = useStore();
+
+const id = Number(route.params.id);
+const unitDetail = computed(() => store.getters["units/allTableDataById"](id));
+
+const unitData = computed(() => {
+  if (unitDetail.value) {
+    return [
+      { label: "ID", value: unitDetail.value.id },
+      { label: "Name", value: unitDetail.value.name },
+      { label: "Description", value: unitDetail.value.description },
+      { label: "Min. Required Age", value: unitDetail.value.age },
+      { label: "Wood Cost", value: unitDetail.value.cost.Wood },
+      { label: "Food Cost", value: unitDetail.value.cost.Food },
+      { label: "Gold Cost", value: unitDetail.value.cost.Gold },
+      { label: "Build Time", value: unitDetail.value.build_time + " seconds" },
+      {
+        label: "Reload Time",
+        value: unitDetail.value.reload_time + " seconds",
+      },
+      { label: "Hit Points", value: unitDetail.value.hit_points },
+      { label: "Attack", value: unitDetail.value.attack },
+      { label: "Accuracy", value: unitDetail.value.accuracy },
+    ];
+  }
+  return [];
+});
 </script>
 
 <template>
-   <p>Unit Detail Page</p>
+  <v-container class="unit-details">
+    <v-row class="unit-details__row">
+      <v-col cols="12" md="8" lg="6" class="unit-details__col">
+        <UnitDetailDisplay :unitData="unitData" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped lang="scss">
+.unit-details {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+
+  &__row {
+    text-align: center;
+    justify-content: center;
+  }
+
+  &__col {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+}
+</style>
