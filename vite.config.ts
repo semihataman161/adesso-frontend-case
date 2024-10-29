21cbd40f2d3bd3891c7af1c6af0f73d693255c53
semihataman161 <semihataman16@gmail.com>
2024-10-29 06:05:17 +0300

    Remove build option inside vite.config to fix vercel deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,6 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true, styles: "sass" })],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: "./src/main.ts",
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
