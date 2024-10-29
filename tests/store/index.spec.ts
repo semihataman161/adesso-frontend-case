bd80bd482e23691ecc5127ebb7fe0d11d604a33a
semihataman161 <semihataman16@gmail.com>
2024-10-29 20:07:51 +0300

    Change test logic inside tests/store/index.spec.ts

--- tests/store/index.spec.ts
+++ tests/store/index.spec.ts
@@ -1,14 +1,8 @@
 import { describe, it, expect } from "vitest";
-import { createStore } from "vuex";
-import unitsModule from "@/store/modules/units";
+import store from "@/store/index";
 
 describe("store/index.ts", () => {
   it("should create store with units module", () => {
-    const store = createStore({
-      modules: {
-        units: unitsModule,
-      },
-    });
     expect(store.state.units).toBeDefined();
   });
 });
